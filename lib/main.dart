0282bb6836c05b92023b731f369b2b15938c80b8
mohand <mohandelsebaey@gmail.com>
2024-07-26 11:57:41 +0300

    app init

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+//import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'screens/home_page.dart';
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context){
+    return const ScreenUtilInit(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
+    );
+  }
+}
+void main(){
+  // check orientation 
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations(
+  //   [
+  //   DeviceOrientation.portraitUp,
+  //   DeviceOrientation.portraitUp,
+  //   ]
+  // );
+  runApp(DevicePreview(builder:(context) => const MyApp(),));
+  //runApp(const MyApp());
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+class HomePage extends StatelessWidget{
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+        title: Text(
+          'Sample AppBar Text',
+          style: TextStyle(
+            fontSize: 25.sp
+          ),
+        ),
+        toolbarHeight: 50.h,
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        color: Colors.grey,
+        child: Text(
+          'Text',
+          style: TextStyle(
+            fontSize: 30.sp,
+          ),
+        ),
+      ),
+    );
+  }
+}
