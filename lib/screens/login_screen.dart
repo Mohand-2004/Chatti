24051a13524ab14529be776f12734a2d34fbb990
mohand <mohandelsebaey@gmail.com>
2024-07-26 13:00:01 +0300

    add chat image

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatelessWidget{
   const LoginScreen({super.key});
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Text('login'),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image(
+              width: 200.r,
+              height: 200.r,
+              image: const AssetImage('assets/images/app icon.png'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
