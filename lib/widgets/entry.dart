44b7696fc23e2681a404ab32aeb2e4b618744e48
mohand <mohandelsebaey@gmail.com>
2024-07-26 15:18:11 +0300

    fix entery widget bugs

--- lib/widgets/entry.dart
+++ lib/widgets/entry.dart
@@ -1,3 +1,4 @@
+import 'dart:io' show Platform; 
 import 'package:flutter/material.dart';
 import 'package:my_app/models/app_colors.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -24,42 +25,42 @@ class _EntryState extends State<Entry>{
     return Container(
       decoration: BoxDecoration(
         color: AppColors.hellBlue,
-        borderRadius: BorderRadius.circular(10.sp)
+        borderRadius: BorderRadius.circular(23.r)
       ),
-      padding: EdgeInsets.all(1.sp),
+      padding: EdgeInsets.all(2.r),
       child: Container(
         alignment: Alignment.center,
-        width: double.infinity,
-        height: double.infinity,
         decoration: BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.circular(9.sp)
+          borderRadius: BorderRadius.circular(20.r)
         ),
         child: Row(
           children: [
-            const Expanded(flex: 1, child: SizedBox()),
+            // icon
             Expanded(
               flex: 15,
-              child: SizedBox(
-                child: Padding(
-                  padding: const EdgeInsets.all(4),
-                  child: SizedBox(
-                    child: FittedBox(
-                      fit: BoxFit.contain,
-                      child: Icon(widget.icon,color: AppColors.hellBlue,),
-                    ),
-                  ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Icon(
+                  size: 40.r,
+                  widget.icon,color: AppColors.hellBlue,
                 ),
               ),
             ),
-            const SizedBox(
+
+            // verticl divider
+            SizedBox(
               height: double.infinity,
               child: VerticalDivider(
-                indent: 10,
-                endIndent: 10,
+                indent: 16,
+                endIndent: 16,
                 color: Colors.grey,
+                thickness: 2.sp,
+                width: 20.w,
               ),
             ),
+
+            // text feild
             Expanded(
               flex: 84,
               child: TextFormField(
@@ -75,18 +76,25 @@ class _EntryState extends State<Entry>{
                   errorStyle: TextStyle(
                     color: Colors.red,
                     fontFamily: 'impact',
-                    fontSize: 6.sp
+                    fontSize: 17.sp
                   ),
                   border: InputBorder.none,
                   hintText: widget.hinttext,
                   hintStyle: const TextStyle(
                     color: Colors.grey
                   ),
-                  contentPadding: const EdgeInsets.only(left: 20)
+                  contentPadding: const EdgeInsets.only(left: 15)
                 ),
                 style: TextStyle(
-                  fontSize: 7.sp,
+                  fontSize: 18.sp,
                 ),
+                onTapOutside: (event) {
+                  // pop the keyboard when foucus outside
+                  // as in iOS the keboard won't pop automaticly
+                  if(Platform.isIOS){
+                    FocusManager.instance.primaryFocus!.unfocus();
+                  }
+                },
               ),
             )
           ],
