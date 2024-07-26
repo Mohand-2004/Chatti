26ffb3d49469c4e805d2c77b97898859eb0658c4
mohand <mohandelsebaey@gmail.com>
2024-07-26 14:17:01 +0300

    add login text

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,10 +12,35 @@ class LoginScreen extends StatelessWidget{
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            // chat image
             Image(
               width: 200.r,
               height: 200.r,
               image: const AssetImage('assets/images/app icon.png'),
+            ),
+
+            // space between
+            const SizedBox(height: 10,),
+
+            // login text row
+            Row(
+              children: [
+                // left space
+                SizedBox(width: 20.w,),
+
+                // login text
+                Text(
+                  'Login to your Account',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'impact',
+                    fontSize: 30.sp
+                  ),
+                ),
+
+                // right space
+                const Spacer(),
+              ],
             ),
           ],
         ),
