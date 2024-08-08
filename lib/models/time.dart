565bc798451a45207b9583dffff62502362b43f2
mohand <mohandelsebaey@gmail.com>
2024-08-08 23:44:36 +0300

    create time model

--- lib/models/time.dart
+++ lib/models/time.dart
@@ -1,27 +1,304 @@
+enum Mode{
+  am,
+  pm,
+}
 class Time{
   late int _seconds;
   late int _minutes;
   late int _hours;
-  Time({required int seconds,required int minutes,required int hours}){
+  late bool _hour24Mode;
+  late Mode _mode;
+  Time({required int seconds,required int minutes,required int hours,bool hour24Mode = false,Mode mode = Mode.am}){
     _seconds = seconds;
     _minutes = minutes;
     _hours = hours;
+    _hour24Mode = hour24Mode;
+    _mode = mode;
   }
   void addSeconds(int seconds){
-    while(seconds != 0){
-      if(_seconds == 60){
-        _seconds = 0;
-        _minutes++;
-
-      }
-      else{
-        _seconds++;
-        seconds--;
-      }
-    }
+    seconds %= 86400;
+    _incrementHours((seconds ~/ 3600));
+    seconds %= 3600;
+    _minutes += (seconds ~/ 60);
+    seconds %= 60;
+    _seconds += seconds;
+  }
+  void addMinutes(int minutes){
+    minutes %= 1440;
+    _incrementHours((minutes ~/ 60));
+    minutes %= 60;
+    _minutes += minutes;
+  }
+  void addHours(int hours){
+    hours %= 24;
+    _incrementHours(hours);
+  }
+  void subtractSeconds(int seconds){
+    seconds %= 86400;
+    _decrementHours((seconds ~/ 3600));
+    seconds %= 3600;
+    _minutes -= (seconds ~/ 60);
+    seconds %= 60;
+    _seconds -= seconds;
+  }
+  void subtractMinutes(int minutes){
+    minutes %= 1440;
+    _decrementHours((minutes ~/ 60));
+    minutes %= 60;
+    _minutes -= minutes;
+  }
+  void subtractHours(int hours){
+    hours %= 24;
+    _decrementHours(hours);
   }
   @override
   String toString(){
-    return "$_hours:$_minutes:$_seconds";
+    return "$_hours:$_minutes:$_seconds ${_hour24Mode ? '' : _mode.name}";
   }
+  void _incrementHours([int hours = 1]){
+    _hours += hours;
+    if(!_hour24Mode && _hours > 12){
+      _hours -= 12;
+      _switchMode();
+    }
+  }
+  void _decrementHours([int hours = 1]){
+    _hours -= hours;
+    if(_hours < 1){
+      _hours += 12;
+      _switchMode();
+    }
+  }
+  void _switchMode(){
+    switch (_mode) {
+      case Mode.am:
+        _mode = Mode.pm;
+        break;
+      case Mode.pm:
+        _mode = Mode.am;
+      default:
+        break;
+    }
+  }
+  bool operator > (Time time){
+    if(_hour24Mode && time.is24){
+      if(_hours > time.hours){
+        return true;
+      }
+      else{
+        if(_hours == time.hours){
+          if(_minutes > time.minutes){
+            return true;
+          }
+          else{
+            if(_minutes == time.minutes){
+              if(_seconds > time.seconds){
+                return true;
+              }
+              else{
+                return _seconds > time.seconds;
+              }
+            }
+            else{
+              return false;
+            }
+          }
+        }
+        else{
+          return false;
+        }
+      }
+    }
+    else if(!time.is24 && !_hour24Mode){
+      if(time.mode == _mode){
+        if(_hours > time.hours){
+          return true;
+        }
+        else{
+          if(_hours == time.hours){
+            if(_minutes > time.minutes){
+              return true;
+            }
+            else{
+              if(_minutes == time.minutes){
+                if(_seconds > time.seconds){
+                  return true;
+                }
+                else{
+                  return _seconds > time.seconds;
+                }
+              }
+              else{
+                return false;
+              }
+            }
+          }
+          else{
+            return false;
+          }
+        }
+      }
+      else{
+        if(_mode == Mode.am && time.mode == Mode.pm){
+          return false;
+        }
+        else{
+          return true;
+        }
+      }
+    }
+    else{
+      if((!_hour24Mode && _mode == Mode.pm ? _hours + 12 : _hours) > (!time.is24 && time.mode == Mode.pm ? time.hours +12 : time.hours)){
+        return true;
+      }
+      else{
+        if((!_hour24Mode && _mode == Mode.pm ? _hours + 12 : _hours) == (!time.is24 && time.mode == Mode.pm ? time.hours +12 : time.hours)){
+          if(_minutes > time.minutes){
+            return true;
+          }
+          else{
+            if(_minutes == time.minutes){
+              if(_seconds > time.seconds){
+                return true;
+              }
+              else{
+                return _seconds > time.seconds;
+              }
+            }
+            else{
+              return false;
+            }
+          }
+        }
+        else{
+          return false;
+        }
+      }
+    }
+  }
+  bool operator < (Time time){
+    if(_hour24Mode && time.is24){
+      if(_hours < time.hours){
+        return true;
+      }
+      else{
+        if(_hours == time.hours){
+          if(_minutes < time.minutes){
+            return true;
+          }
+          else{
+            if(_minutes == time.minutes){
+              if(_seconds < time.seconds){
+                return true;
+              }
+              else{
+                return _seconds < time.seconds;
+              }
+            }
+            else{
+              return false;
+            }
+          }
+        }
+        else{
+          return false;
+        }
+      }
+    }
+    else if(!time.is24 && !_hour24Mode){
+      if(time.mode == _mode){
+        if(_hours < time.hours){
+          return true;
+        }
+        else{
+          if(_hours == time.hours){
+            if(_minutes < time.minutes){
+              return true;
+            }
+            else{
+              if(_minutes == time.minutes){
+                if(_seconds < time.seconds){
+                  return true;
+                }
+                else{
+                  return _seconds < time.seconds;
+                }
+              }
+              else{
+                return false;
+              }
+            }
+          }
+          else{
+            return false;
+          }
+        }
+      }
+      else{
+        if(_mode == Mode.am && time.mode == Mode.pm){
+          return false;
+        }
+        else{
+          return true;
+        }
+      }
+    }
+    else{
+      if((!_hour24Mode && _mode == Mode.pm ? _hours + 12 : _hours) < (!time.is24 && time.mode == Mode.pm ? time.hours +12 : time.hours)){
+        return true;
+      }
+      else{
+        if((!_hour24Mode && _mode == Mode.pm ? _hours + 12 : _hours) == (!time.is24 && time.mode == Mode.pm ? time.hours +12 : time.hours)){
+          if(_minutes < time.minutes){
+            return true;
+          }
+          else{
+            if(_minutes == time.minutes){
+              if(_seconds < time.seconds){
+                return true;
+              }
+              else{
+                return _seconds < time.seconds;
+              }
+            }
+            else{
+              return false;
+            }
+          }
+        }
+        else{
+          return false;
+        }
+      }
+    }
+  }
+  bool isEqual(Time time){
+    if(_hour24Mode == time.is24){
+      if(_hour24Mode && time.is24){
+        return (_hours == time.hours && _minutes == time.minutes && _seconds == time.seconds);
+      }
+      else{
+        if(time.mode != _mode){
+          return false;
+        }
+        else{
+          return (_hours == time.hours && _minutes == time.minutes && _seconds == time.seconds);
+        }
+      }
+    }
+    else{
+      return (((!_hour24Mode && _mode == Mode.pm) ? _hours + 12 : _hours) == ((!time.is24 && time.mode == Mode.pm) ? time.hours + 12 : time.hours) && _minutes == time.minutes && _seconds == time.seconds);
+    }
+  }
+  bool operator >= (Time time){
+    return this > time || isEqual(time);
+  }
+  bool operator <= (Time time){
+    return this < time || isEqual(time);
+  }
+  int get seconds => _seconds;
+  int get minutes => _minutes;
+  int get hours => _hours;
+  Mode get mode => _mode;
+  bool get is24 => _hour24Mode;
 }
